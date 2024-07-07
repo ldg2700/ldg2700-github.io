e477f9ab9af656aeeca558b48fe46b864a5fb006
ldg2700 <149889066+ldg2700@users.noreply.github.com>
2024-07-08 01:57:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function scrollToElement(elementSelector, instance = 0) {
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
 
 link1.addEventListener('click', () => {
     scrollToElement('.header');
@@ -23,4 +24,8 @@ link2.addEventListener('click', () => {
 
 link3.addEventListener('click', () => {
     scrollToElement('.header', 2);
+});
+
+link4.addEventListener('click', () => {
+    scrollToElement('.header', 3);
 });
